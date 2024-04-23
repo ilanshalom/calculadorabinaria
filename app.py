1b552391ced628482dd2f6fa228779980bddfdb3
Ilan Garcia <ilanelvaron24@gmail.com>
2024-04-23 16:32:35 -0300

    calculadora no ar

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask, request, render_template
 
+erro = 'Erro: Inserir somente números binários!'
+
 app = Flask(__name__)
 
 def binario_para_decimal(binario):
     return int(binario, 2)
 
 def decimal_para_binario(decimal):
-    return bin(decimal)[2:]
+    if decimal < 0:
+        return '-' + bin(abs(decimal))[2:]
+    else:
+        return bin(decimal)[2:]
+
+def soma_binaria(binario1, binario2):
+    decimal1 = binario_para_decimal(binario1)
+    decimal2 = binario_para_decimal(binario2)
+    resultado_decimal = decimal1 + decimal2
+    return decimal_para_binario(resultado_decimal)
+
+def subtracao_binaria(binario1, binario2):
+    decimal1 = binario_para_decimal(binario1)
+    decimal2 = binario_para_decimal(binario2)
+    resultado_decimal = decimal1 - decimal2
+    return decimal_para_binario(resultado_decimal)
+
+def multiplicacao_binaria(binario1, binario2):
+    decimal1 = binario_para_decimal(binario1)
+    decimal2 = binario_para_decimal(binario2)
+    resultado_decimal = decimal1 * decimal2
+    return decimal_para_binario(resultado_decimal)
 
 @app.route('/')
 def index():
@@ -14,23 +37,22 @@ def index():
 
 @app.route('/calcular', methods=['POST'])
 def calcular():
-    operacao = request.form['operacao']
-    num1 = request.form['num1']
-    num2 = request.form['num2']
+    try:
+        operacao = request.form['operacao']
+        num1 = request.form['num1']
+        num2 = request.form['num2']
 
-    decimal1 = binario_para_decimal(num1)
-    decimal2 = binario_para_decimal(num2)
+        if operacao == 'soma':
+            resultado = soma_binaria(num1, num2)
+        elif operacao == 'subtracao':
+            resultado = subtracao_binaria(num1, num2)
+        elif operacao == 'multiplicacao':
+            resultado = multiplicacao_binaria(num1, num2)
+        else:
+            resultado = "Operação inválida"
 
-    if operacao == 'soma':
-        resultado = decimal_para_binario(decimal1 + decimal2)
-    elif operacao == 'subtracao':
-        resultado = decimal_para_binario(decimal1 - decimal2)
-    elif operacao == 'multiplicacao':
-        resultado = decimal_para_binario(decimal1 * decimal2)
-    else:
-        resultado = "Operação inválida"
-
-    return render_template('resultado.html', resultado=resultado)
-
+        return render_template('index.html', resultado=resultado)
+    except:
+        return render_template('index.html', resultado=erro)
 if __name__ == '__main__':
     app.run(debug=True)
